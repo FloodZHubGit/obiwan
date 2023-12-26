c7a791d1c8bbc8d3a8c68895c705dfb437baf246
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-12-26 19:36:02 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Physics } from "@react-three/rapier";
+import { KeyboardControls } from "@react-three/drei";
+import { EcctrlJoystick } from "ecctrl";
+import { useEffect, useState } from "react";
+
+const EcctrlJoystickControls = () => {
+  const [isTouchScreen, setIsTouchScreen] = useState(false);
+  useEffect(() => {
+    // Check if using a touch control device, show/hide joystick
+    if ("ontouchstart" in window || navigator.maxTouchPoints > 0) {
+      setIsTouchScreen(true);
+    } else {
+      setIsTouchScreen(false);
+    }
+  }, []);
+  return <>{isTouchScreen && <EcctrlJoystick buttonNumber={5} />}</>;
+};
 
 function App() {
+  const keyboardMap = [
+    { name: "forward", keys: ["ArrowUp", "KeyW"] },
+    { name: "backward", keys: ["ArrowDown", "KeyS"] },
+    { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+    { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+    { name: "jump", keys: ["Space"] },
+    { name: "run", keys: ["Shift"] },
+    { name: "action1", keys: ["1"] },
+    { name: "action2", keys: ["2"] },
+    { name: "action3", keys: ["3"] },
+    { name: "action4", keys: ["KeyF"] },
+  ];
+
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <EcctrlJoystickControls />
+      <Canvas
+        shadows
+        camera={{ position: [3, 3, 3], fov: 65, near: 0.1, far: 1000 }}
+        onPointerDown={(e) => {
+          if (e.pointerType === "mouse") {
+            e.target.requestPointerLock();
+          }
+        }}
+      >
+        <color attach="background" args={["#ececec"]} />
+        <Physics>
+          <KeyboardControls map={keyboardMap}>
+            <Experience />
+          </KeyboardControls>
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,60 @@
-import { OrbitControls } from "@react-three/drei";
+import { Grid, OrbitControls } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
+import Ecctrl, { EcctrlAnimation } from "ecctrl";
+import { Astronaut } from "./Astronaut";
+import { Demon } from "./Demon";
 
 export const Experience = () => {
+  const characterURL = "./models/Demon.glb";
+
+  const animationSet = {
+    idle: "CharacterArmature|Idle",
+    walk: "CharacterArmature|Walk",
+    run: "CharacterArmature|Run",
+    jump: "CharacterArmature|Jump",
+    jumpIdle: "CharacterArmature|Jump_Idle",
+    jumpLand: "CharacterArmature|Jump_Land",
+    fall: "CharacterArmature|Duck",
+    action1: "CharacterArmature|Wave",
+    action2: "CharacterArmature|Death",
+    action3: "CharacterArmature|HitReact",
+    action4: "CharacterArmature|Punch",
+  };
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={5} />
+
+      <Grid
+        args={[300, 300]}
+        sectionColor={"lightgray"}
+        cellColor={"gray"}
+        position={[0, 5.1, 0]}
+        userData={{ camExcludeCollision: true }} // this won't be collide by camera ray
+      />
+
+      <directionalLight position={[0, 0, 0]} intensity={10} castShadow />
+
+      <Ecctrl debug animated>
+        <EcctrlAnimation
+          characterURL={characterURL}
+          animationSet={animationSet}
+        >
+          <Demon scale={0.5} position={[0, -0.9, 0]} />
+        </EcctrlAnimation>
+      </Ecctrl>
+
+      <RigidBody type="fixed">
+        <mesh
+          position-y={-20}
+          rotation-x={-Math.PI / 2}
+          scale={50}
+          receiveShadow
+        >
+          <boxGeometry />
+          <meshBasicMaterial color="lightgreen" />
+        </mesh>
+      </RigidBody>
     </>
   );
 };
